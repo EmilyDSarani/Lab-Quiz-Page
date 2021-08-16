fedc94edc06f802c27f2e79246e05e0d00a6dd00
EmilyDSarani <emily.sarani@gmail.com>
2021-08-16 17:47:31 -0500

    test redo

--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,5 @@
 // IMPORT MODULES under test here:
-import { example } from '../example.js';
+
 import { yessir } from './utils.js';
 const test = QUnit.test;
 
@@ -7,7 +7,7 @@ test('time to test a function', (expect) => {
     
     const expected = true;
     
-    const actual = yessir('yeeees!');
+    const actual = yessir('yeeees');
 
     expect.equal(actual, expected);
 });
